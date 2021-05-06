3126feb4bd66d4a49ac174f909ff9b9f828854cb
liangchao <1005223241@qq.com>
2021-05-06 16:02:57 +0800

    单机eureka实例，provider-8001中测试注册eureka

--- SpringCloud-provider-8001/src/main/java/com/learn/provider/controller/DeptController.java
+++ SpringCloud-provider-8001/src/main/java/com/learn/provider/controller/DeptController.java
@@ -3,6 +3,8 @@ package com.learn.provider.controller;
 import com.learn.api.pojo.Dept;
 import com.learn.provider.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +19,32 @@ import java.util.List;
 public class DeptController {
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client; // 用于服务发现，获取eureka注册信息中有所有微服务的信息
+
+    /**
+     * 测试服务发现，查看当前eureka集群中所有服务的信息
+     * @return
+     */
+    @GetMapping("/dept/discovery")
+    public Object discovery(){
+        List<String> services = client.getServices();
+        System.out.println("当前eureka中注册的服务清单为："+services);
+
+        String serviceId = "SPRINGCLOUD-PROVIDER-8001-SPRING-APPLICATION";
+        System.out.println("获取serviceId为：" + serviceId + "的服务信息");
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-8001-SPRING-APPLICATION");
+        for(ServiceInstance i : instances){
+            System.out.println(
+                    i.getHost() + "\t"
+                    + i.getPort() + "\t"
+                    + i.getUri() + "\t"
+                    + i.getServiceId()
+            );
+        }
+        return this.client;
+    }
 
     @PostMapping("/dept/add")
     public Boolean addDept(@RequestBody  Dept dept) {
