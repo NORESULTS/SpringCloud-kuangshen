6f6688c92560d8b079c2a2b2b47b841f10bf6074
liangchao <1005223241@qq.com>
2021-05-17 19:09:48 +0800

    新增Ribbon自定义算法

--- SpringCloud-consumer-80/src/main/java/com/learn/consumer/SpringCloudConsumer80Starter.java
+++ SpringCloud-consumer-80/src/main/java/com/learn/consumer/SpringCloudConsumer80Starter.java
@@ -1,8 +1,10 @@
 package com.learn.consumer;
 
+import com.learn.consumer.config.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author liangchao
@@ -10,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient // Ribbon和Eureka整合后，客户端可以直接通过提供者的服务名去调用，无需指定提供者的IP地址与端口号
+@RibbonClient(name="SpringCloud-provider-ribbon-spring-application",configuration = MyRule.class) // 配置当服务启动时，就加载针对目标提供者提供自定义的负载均衡算法
 public class SpringCloudConsumer80Starter {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudConsumer80Starter.class,args);
--- SpringCloud-consumer-80/src/main/java/com/learn/consumer/config/ConfigBean.java
+++ SpringCloud-consumer-80/src/main/java/com/learn/consumer/config/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.learn.consumer.config;
 
+import com.netflix.loadbalancer.IRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +19,19 @@ public class ConfigBean {
      * @return
      */
     @Bean
-    @LoadBalanced // 通过这个注解，实现在客户端消费者要去通过Rest访问提供者时，去做ribbon负载均衡
+    @LoadBalanced // 通过这个注解，实现在客户端消费者要去通过Rest访问提供者时，去做ribbon负载均衡 .此处只起到开启负载均衡的开关作用
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
+
+    /**
+     * 将Ribbon默认的均衡算法，从 轮询 RoundRobinRule 改为自定义
+     * 与在Starter上指定对应服务的自定义算法，作用相同
+     * @return
+     */
+    @Bean
+    public IRule myRule(){
+        return new MyRule();
+    }
+
 }
